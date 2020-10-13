87a658593847874e6f9f131d57c5ba4c94740024
Adrian <alihs@redcrossblood.org.au>
2020-10-13 21:18:28 +1100

    replacing API url with environment variable

--- src/axios.js
+++ src/axios.js
@@ -4,7 +4,8 @@ const instance = axios.create({
 	// baseURL: 'http://localhost:5001/translatorapi-1344d/australia-southeast1/api',
 	// baseURL: 'https://australia-southeast1-translatorapi-1344d.cloudfunctions.net/api', // API url from cloud function
 	// baseURL: 'http://localhost:9000',
-	baseURL: 'https://transmate-api-mern.herokuapp.com',
+	// baseURL: 'https://transmate-api-mern.herokuapp.com',
+	baseURL: process.env.TRANSMATEAPI
 });
 
 export default instance;
