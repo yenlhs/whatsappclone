fb9cc462014d8bda9aafb259096b7ac23b7004e6
Adrian <alihs@redcrossblood.org.au>
2020-10-14 15:28:08 +1100

    removed message prop

--- src/App.js
+++ src/App.js
@@ -4,12 +4,6 @@ import Sidebar from './Sidebar';
 import Chat from './Chat'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-const messages = [{
-  name: 'Adrian',
-  message: 'Something',
-  timestamp: 'adfasdf',
-  received: false
-}]
 
 function App() {
   return (
@@ -20,7 +14,7 @@ function App() {
 						<div className='app__body'>
 							{/* Sidebar */}
 							<Sidebar />
-							<Chat messages={messages} />
+							<Chat />
 						</div>
 					</Route>
 				</Switch>
